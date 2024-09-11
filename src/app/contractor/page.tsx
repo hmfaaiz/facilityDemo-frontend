08de547c8e88a3d2838a880eadca3d17295c6566
faaiz <email>
2024-09-11 23:53:23 +0500

    done

--- src/app/contractor/page.tsx
+++ src/app/contractor/page.tsx
@@ -25,7 +25,7 @@ const FormLayout = () => {
    
       <button
         onClick={handleOpenModal}
-        className="btn btn-primary mb-4"
+        className="btn btn-primary m-4 inline-flex items-center justify-center rounded-md border border-slate-950 px-10 py-4 text-center font-medium text-black hover:bg-black hover:text-white lg:px-8 xl:px-10"
       >
         Add Contractor
       </button>
